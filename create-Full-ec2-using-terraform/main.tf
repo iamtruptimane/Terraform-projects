c4e7d6e15f10cb8fa5d9d1d79abe36f878fb4fba
Your Name <you@example.com>
2023-11-07 22:06:11 +0530

    day1 of terraform project

---
+++ create-Full-ec2-using-terraform/main.tf
@@ -0,0 +1,85 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone_id    = "use1-az1"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "sub2" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone_id    = "use1-az2"
+  map_public_ip_on_launch = true
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.RT.id
+}
+resource "aws_security_group" "websg" {
+    name = "web"
+    vpc_id = aws_vpc.myvpc.id
+
+    ingress {
+        description = "HTTP from VPC"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "Web-sg"
+    }  
+}
+
+resource "aws_s3_bucket" "example" {
+    bucket = "trupti_terraform_2023_project"  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
