244e8e4e376fe0f6ae37cb9b83246981626e2fe5
iamtruptimane <iamtruptimane@gmail.com>
2023-11-21 09:43:55 +0530

    added main.tf file

---
+++ 07-deploying-vpc/main.tf
@@ -0,0 +1,34 @@
+    terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "5.9.0"
+        }
+    }
+    }
+
+    provider "aws" {
+    region = "us-west-2"
+    }
+
+    resource "aws_vpc" "lab_vpc" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "ca-lab-vpc"
+    }
+    }
+
+    output "id" {
+    description = "VPC ID"
+    value       = aws_vpc.lab_vpc.id
+    }
+    output "route_table_id" {
+    description = "Route Table ID associated with this VPC"
+    value       = aws_vpc.lab_vpc.main_route_table_id
+    }
+    output "security_group_id" {
+    description = "Default Security Group ID"
+    value       = aws_vpc.lab_vpc.default_security_group_id
+    }
+
